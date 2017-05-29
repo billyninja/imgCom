11f751bef50bb934f341ec6ccb3b3a0ccaa89879
billyninja <joaoeduardocm@gmail.com>
2017-05-29 20:36:23 -0300

    fixing the main

--- main.go
+++ main.go
@@ -1,43 +1,18 @@
 package main 
 
 import(
-    "os"
-    "fmt"
-    "github.com/veandco/go-sdl2/sdl"
+    "github.com/billyninja/imgCom/xmg"
     "github.com/veandco/go-sdl2/sdl_image"
 )
 
 
-func GetRendererFromSurf(surf *sdl.Surface) (*sdl.Renderer, error) {
-    rdr, err := sdl.CreateSoftwareRenderer(surf)
-    if err != nil {
-        os.Exit(2)
-    }
-
-    return rdr, err
-}
-
-func LoadImg(filename string) (*sdl.Surface, error) {
-    image, err := img.Load(filename)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to load PNG: %s\n", err)
-        return nil, err
-    }
-    
-    return image, err
-}
-
-
-
-func GetRendererFromFilename(filename string) (*sdl.Renderer, error) {
-    surfImg, err := LoadImg(filename)
-    rdr, err := GetRendererFromSurf(surfImg)
-
-    return rdr, err
-}
-
-
 func main() {
-    rdr, err := GetRendererFromFilename("sample_media/images/strix-nebulosa.jpg")
-    println(rdr, err)
+    surf, rdr, err := xmg.GetRendererFromFilename("sample_media/images/strix-nebulosa.jpg")
+    err = img.SavePNG(surf, "out/strix-nebulosa.png")
+    man, err := xmg.NewManager(
+        []string{"sample_media/images"},
+        []string{"sample_media/fonts"},
+    )
+
+    println(man, rdr, err)
 }
